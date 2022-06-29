0921b13f78aaabaae1c81e2d13e639bd937c0bdb
ConfusedPandaEvan <hwl278@nyu.edu>
2022-06-30 04:29:54 +0900

    CICD update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,9 @@
 module.exports = {
   apps: [
     {
-      name: 'aws-codedeploy',
-      script: 'npx',
-      args: 'serve -s build -l 3000 -n',
-      interpreter: 'none',
-      env: {
-        NODE_ENV: 'development',
-      },
+      name: 'onvi',
+      script: 'npm',
+      args: 'start',
     },
   ],
 };
