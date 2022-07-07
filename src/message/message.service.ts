62641cd952ce8d36a6742104ec2b79b75238a82c
jkim9409 <92039854+jkim9409@users.noreply.github.com>
2022-07-07 22:19:09 +0900

    Update message.service.ts

--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMessageDto } from './dto/create-message.dto';
-import { UpdateMessageDto } from './dto/update-message.dto';
 
 @Injectable()
 export class MessageService {
