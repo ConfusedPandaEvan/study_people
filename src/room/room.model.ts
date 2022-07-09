8e017bb3547702f099ebd4b30023052c9911f26c
ConfusedPandaEvan <44895166+ConfusedPandaEvan@users.noreply.github.com>
2022-07-09 16:51:15 +0900

    Update room.model.ts

--- src/room/room.model.ts
+++ src/room/room.model.ts
@@ -3,16 +3,16 @@ import { User, UserSchema } from 'src/schemas/user.Schema';
 
 export const RoomSchema = new mongoose.Schema({
   users: { type: [String], default: undefined, required: true },
-  title: { type: String,},
-  content: { type: String,},
-  password: { type: String,},
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  password: { type: String, required: true },
   //change hashtag to hashtag schema
   hashtags: { type: [String], default: undefined },
-  openKakao: { type: String, },
-  maxPeople: { type: Number, },
-  imageLocation: { type: String,  },
-  createdAt: { type: Date, },
-  lastVisited: { type: Date, },
+  openKakao: { type: String, required: true },
+  maxPeople: { type: Number, required: true },
+  imageLocation: { type: String, required: true },
+  createdAt: { type: Date, required: true },
+  lastVisited: { type: Date, required: true },
 });
 
 export interface Room extends mongoose.Document {
