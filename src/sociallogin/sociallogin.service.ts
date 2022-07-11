e72bf806b885fc4865c0480f457d98e784e05ab2
jkim9409 <92039854+jkim9409@users.noreply.github.com>
2022-07-11 14:33:05 +0900

    Update sociallogin.service.ts

--- src/sociallogin/sociallogin.service.ts
+++ src/sociallogin/sociallogin.service.ts
@@ -12,7 +12,8 @@ export class SocialloginService {
     async kakaoLoginMain(@Query() query) {
         const kakao = {
             clientid: '968fe442549959a4ab2bb530f508c889',
-            redirectUri: 'http://localhost:3000/main',
+            // redirectUri: 'http://localhost:3000/main',
+            redirectUri: 'http://13.125.58.110:3000/main',
             // 수정 필요 redirectUri: 'http://13.125.58.110:3000/main',
         };
 
