0ee7c3b15a7e50b299e76988b11691107fb81b9c
jkim9409 <92039854+jkim9409@users.noreply.github.com>
2022-07-11 18:10:11 +0900

    Update auth.middleware.ts

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -20,33 +20,6 @@ interface JwtPayload {
 }
 
 @Injectable()
-<<<<<<< Updated upstream
-export class Authmiddleware implements NestMiddleware {
-  constructor(@InjectModel('User') private userModel: Model<UserDocument>) {}
-  use(req: Request, res: Response, next: NextFunction) {
-    console.log('middleware passed');
-    const { authorization } = req.headers;
-    console.log(authorization);
-    if (!authorization) {
-      throw new HttpException(
-        {
-          status: HttpStatus.BAD_REQUEST,
-          errorMessage: '로그인 후 이용하세요!',
-        },
-        HttpStatus.BAD_REQUEST,
-      );
-    }
-    const [tokenType, tokenValue] = authorization.split(' ');
-    if (tokenType !== 'Bearer') {
-      console.log('error1');
-      throw new HttpException(
-        {
-          status: HttpStatus.BAD_REQUEST,
-          errorMessage: '로그인 후 이용하세요!',
-        },
-        HttpStatus.BAD_REQUEST,
-      );
-=======
 export class Authmiddleware implements NestMiddleware{
     constructor(@InjectModel('User') private userModel: Model<UserDocument>){}
         use(req: Request,res: Response, next: NextFunction) {
@@ -99,33 +72,4 @@ export class Authmiddleware implements NestMiddleware{
                 );
               }
         next()
-        
->>>>>>> Stashed changes
-    }
-    try {
-      // Mykey Value needs to be changed
-      // const userid = jwt.verify(tokenValue, 'MyKey');
-      // const loginUser = await SocialloginService.findUser({ _id:userid });
-      // res.locals.user = user
-      // next();
-      console.log('error2');
-      const token = jwt.verify(tokenValue, 'MyKey') as JwtPayload;
-      console.log('valid token: ', token);
-      this.userModel.findOne({ _id: token.userId }).then((user) => {
-        res.locals.user = user;
-        console.log(res.locals);
-        console.log('after next');
-      });
-    } catch (err) {
-      console.log('error3');
-      throw new HttpException(
-        {
-          status: HttpStatus.BAD_REQUEST,
-          errorMessage: '로그인 후 이용하세요!',
-        },
-        HttpStatus.BAD_REQUEST,
-      );
-    }
-    next();
-  }
-}
+            }}
