b8bfac9963dd5f65e4c708dc9a245bafd837d537
jkim9409 <92039854+jkim9409@users.noreply.github.com>
2022-07-11 12:26:55 +0900

    feat social login uri

--- src/sociallogin/sociallogin.controller.ts
+++ src/sociallogin/sociallogin.controller.ts
@@ -6,9 +6,12 @@ export class SocialloginController {
   constructor(private readonly SocialloginService: SocialloginService) {}
 
   @Get('main')
-  @Redirect('http://stupy.co.kr/kakao/login', 301)
+  // @Redirect('http://stupy.co.kr/kakao/login', 301)
+  @Redirect('http://localhost:3000/kakao/login', 301)
   async kakaoLoginMain(@Query() query: string) {
     const { token } = await this.SocialloginService.kakaoLoginMain(query);
-    return { url: `http://stupy.co.kr/kakao/login?token=${token}` };
+    //나중에 다시 바꿔야함
+    // return { url: `http://stupy.co.kr/kakao/login?token=${token}` };
+    return { url: `http://localhost:3000/kakao/login?token=${token}` };
   }
 }
