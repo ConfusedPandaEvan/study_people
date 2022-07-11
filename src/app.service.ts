e4786bf8a38975f5c632770e97922553440edcc3
jkim9409 <92039854+jkim9409@users.noreply.github.com>
2022-07-11 14:14:54 +0900

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'connection test123, check CreateRoom';
+    return 'connection test123, check CreateRoom,check middleware';
   }
 }
