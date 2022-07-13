24c48307752e4ffce0043b584959ea79b97f1b17
jkim9409 <92039854+jkim9409@users.noreply.github.com>
2022-07-13 13:37:11 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin:"http://stupy.co.kr",
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-});
+  app.enableCors();
+//   app.enableCors({
+//     origin:"http://stupy.co.kr",
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+//     credentials: true,
+// });
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
