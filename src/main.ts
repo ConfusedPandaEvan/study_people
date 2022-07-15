d95a5b67d6b48e2ecaccbbd37cd667acd9c5b35f
jkim9409 <92039854+jkim9409@users.noreply.github.com>
2022-07-15 16:52:44 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,13 @@ import { AppModule } from './app.module';
 import * as fs from 'fs';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('./private.key'),
-    cert: fs.readFileSync('./private.crt')
+  // const httpsOptions = {
+  //   key: fs.readFileSync('./private.key'),
+  //   cert: fs.readFileSync('./private.crt')
 
-  }
-  const app = await NestFactory.create(AppModule,{httpsOptions});
+  // }
+  // const app = await NestFactory.create(AppModule,{httpsOptions});
+  const app = await NestFactory.create(AppModule)
   app.enableCors();
 //   app.enableCors({
 //     origin:"http://stupy.co.kr",
