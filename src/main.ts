c85481c0c5bd7df2bff1601496b5c335d56c6743
ConfusedPandaEvan <hwl278@nyu.edu>
2022-07-15 17:51:22 +0900

    Revert to before https

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as fs from 'fs';
 
 async function bootstrap() {
-  // const httpsOptions = {
-  //   key: fs.readFileSync('./private.key'),
-  //   cert: fs.readFileSync('./private.crt')
-
-  // }
-  // const app = await NestFactory.create(AppModule,{httpsOptions});
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
 //   app.enableCors({
 //     origin:"http://stupy.co.kr",
