4229355e843f2ff30614b920c2989134874116fa
ConfusedPandaEvan <hwl278@nyu.edu>
2022-07-17 04:39:50 +0900

    인증서 확인용

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,11 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @Get('/.well-known/pki-validation/010E487D5E54DDAB56FEEBAF2F4EAB37.txt')
+  wellknown(@Res() res: any) {
+    res.sendFile(
+      __dirname +
+        '/.well-known/pki-validation/010E487D5E54DDAB56FEEBAF2F4EAB37.txt',
+    );
+  }
 }
