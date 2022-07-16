480b125484fa6be519c2e4894a2f84bd0f9912ea
ConfusedPandaEvan <hwl278@nyu.edu>
2022-07-16 18:25:11 +0900

    revert main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as https from 'https';
-import * as http from 'http';
-import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  //   app.enableCors({
-  //     origin:"http://stupy.co.kr",
-  //     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  //     credentials: true,
-  // });
-
-  const server = express();
+//   app.enableCors({
+//     origin:"http://stupy.co.kr",
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+//     credentials: true,
+// });
 
   app.useGlobalPipes(new ValidationPipe());
-  await app.init();
-
-  // http.createServer(server).listen(3000);
-  await https.createServer(server).listen(443);
+  await app.listen(3000);
 }
 bootstrap();
