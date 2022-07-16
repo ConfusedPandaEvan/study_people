3831f2cc5f67525a15ca9d002e74af39ef859771
ConfusedPandaEvan <hwl278@nyu.edu>
2022-07-17 04:12:05 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,11 @@ import * as https from 'https';
 import * as express from 'express';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync(__dirname + '/private.key', 'utf-8'),
-    cert: fs.readFileSync(__dirname + '/certificate.crt', 'utf-8'),
-    ca: fs.readFileSync(__dirname + '/ca_bundle.crt', 'utf-8'),
-  };
+  // const httpsOptions = {
+  //   key: fs.readFileSync(__dirname + '/private.key', 'utf-8'),
+  //   cert: fs.readFileSync(__dirname + '/certificate.crt', 'utf-8'),
+  //   ca: fs.readFileSync(__dirname + '/ca_bundle.crt', 'utf-8'),
+  // };
   const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.enableCors();
@@ -24,15 +24,15 @@ async function bootstrap() {
   await app.init();
 
   const httpServer = http.createServer(server);
-  const httpsServer = https.createServer(httpsOptions, server);
+  // const httpsServer = https.createServer(httpsOptions, server);
   // app.useWebSocketAdapter(new SocketIoAdapter(httpServer));
 
   httpServer.listen(3000, () => {
     console.log('3000번 포트로 서버가 켜졌어요.');
   });
-  httpsServer.listen(3001, () => {
-    console.log('3001번 포트로 서버가 켜졌어요.');
-  });
+  // httpsServer.listen(3001, () => {
+  //   console.log('3001번 포트로 서버가 켜졌어요.');
+  // });
 }
 
 bootstrap();
