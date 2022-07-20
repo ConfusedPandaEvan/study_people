9a7b20cafb1bf3a76c23e6a6bc5b045571681158
jkim9409 <92039854+jkim9409@users.noreply.github.com>
2022-07-20 19:54:12 +0900

    Update message.gateway.ts

--- src/message/message.gateway.ts
+++ src/message/message.gateway.ts
@@ -18,8 +18,8 @@ import { User } from 'src/users/user.Schema';
 @WebSocketGateway({
   transports: ['websocket','polling'],
   cors:{
-    // origin:'*',
-    origin:["http://stupy.co.kr","https://stupy.co.kr","https://localhost:3000","http://localhost:3000"],
+    origin:'*',
+    // origin:["http://stupy.co.kr","https://stupy.co.kr","https://localhost:3000","http://localhost:3000"],
     methods: ["GET","POST"],
     credentials: false
   },
