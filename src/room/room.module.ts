34b8e349738abd540d69723cc908a9bad9955b06
jkim9409 <92039854+jkim9409@users.noreply.github.com>
2022-08-03 13:25:57 +0900

    add schmea to room module

--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -7,12 +7,15 @@ import { RoomService } from './room.service';
 import { UserSchema } from 'src/users/user.Schema';
 import { SocialloginService } from 'src/sociallogin/sociallogin.service';
 import { RoomSearchService } from './roomSearch.service';
-
+import { ChatSchema } from 'src/chats/chat.Schema';
+import { TimeSchema } from 'src/times/time.Schema';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Room', schema: RoomSchema }]),
     MongooseModule.forFeature([{ name: 'Hashtag', schema: HashtagSchema }]),
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    MongooseModule.forFeature([{ name: 'Chat', schema: ChatSchema }]),
+    MongooseModule.forFeature([{ name: 'Time', schema: TimeSchema }]),
   ],
   controllers: [RoomController],
   providers: [RoomService, SocialloginService, RoomSearchService],
